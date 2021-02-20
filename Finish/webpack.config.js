c213ddd0094795170bd5a1778d2c19ea35bffdaf
Brice Wilson <brice@5wilsons.net>
2021-02-20 16:56:17 -0500

    initial commit

---
+++ Finish/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './app/app.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: './app/bundle.js'
+  }
+};
